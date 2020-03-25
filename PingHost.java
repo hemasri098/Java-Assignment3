ab3f2a9c6da9007b6691393a88d91f5e910b1316
hemasri098 <59275471+hemasri098@users.noreply.github.com>
2020-03-25 16:32:01 +0530

    added loggers

--- PingHost.java
+++ PingHost.java
@@ -1,48 +1,74 @@
-package com.company;
+package com.company.JavaAssignment3;
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.logging.Logger;
 
 public class PingHost
 {
-    // method for finding the ping statics of website
-    public static void commands(ArrayList<String> commandList) throws Exception
-    {
+    //to find median of the time taken for the packets
+    public static void findMedian(ArrayList<String> commandList) throws Exception {
+        Logger LOGGER = Logger.getAnonymousLogger();
+        ArrayList<BufferedReader> packets = readPackets(commandList);
+        BufferedReader input = packets.get(0);
+        BufferedReader error = packets.get(1);
+        String packet = null;
+        // to store packets time
+        ArrayList<Double> times = new ArrayList<Double>();
+        try {
+            while((packet = input.readLine()) != null)
+            {
+
+                String [] line = packet.split(" ");
+                //seperating time from each line
+                if(line[line.length - 2].length() > 5)
+                {
+                    times.add(Double.parseDouble(line[line.length - 2].substring(5)));
+                }
+                Collections.sort(times);
+                //finding median
+                if(times.size() > 0) {
+                    //if times are of even length
+                    if (times.size() % 2 != 0) {
+                        double median = times.size() / 2;
+                        LOGGER.info(String.valueOf(median));
+                    }
+                    else {
+                        double median = (times.get(times.size() / 2) + times.get(times.size() / 2 - 1)) / 2;
+                        LOGGER.info(String.valueOf(median));
+                    }
+                }
+
+            }
+            //if we doesn't receive any packets while executing ping command
+            while ((packet = error.readLine()) != null) {
+                LOGGER.info(packet);
+            }
+        }
+        catch (Exception e) {
+            LOGGER.info("Packets stopped");
+        }
+    }
+
+    // method for reading the ping statics of website
+    public static ArrayList<BufferedReader> readPackets(ArrayList<String> commandList) throws Exception {
+
         // creating the sub process, execute system command
         ProcessBuilder build = new ProcessBuilder(commandList);
         Process process = build.start();
 
-        // to read the output
+        // to read the packets return from ping command
         BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedReader Error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-        String s = null;
-        ArrayList<String> packets = new ArrayList<String>();
-        ArrayList<Double> time = new ArrayList<Double>();
-        while((s = input.readLine()) != null)
-        {
-            String [] pac = s.split(" ");
-            if(pac[pac.length - 2].length() > 5)
-            {
-                time.add(Double.parseDouble(pac[pac.length - 2].substring(5)));
-            }
-            Collections.sort(time);
-            if(time.size() > 0) {
-                if (time.size() % 2 != 0)
-                    System.out.println(time.get(time.size() / 2));
-                else
-                    System.out.println((time.get(time.size() / 2) + time.get(time.size() / 2 - 1)) / 2);
-            }
-
-        }
-
-        while((s = Error.readLine()) != null)
-        {
-            System.out.println(s);
-        }
+        BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        ArrayList<BufferedReader> packets = new ArrayList<BufferedReader>();
+        packets.add(input);
+        packets.add(error);
+        return packets;
 
     }
 
@@ -50,11 +76,9 @@ public class PingHost
     {
         // creating list for commands
         ArrayList<String> commandList = new ArrayList<String>();
-
         commandList.add("ping"); //command to ping ip address is added into list
-
         commandList.add("www.yahoo.com"); // host address
+        findMedian(commandList);
 
-        commands(commandList);
     }
 }
